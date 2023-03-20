1a63ee225259f7d0fab43a018514f2b94f3678ac
ghazi <ghazi.gq@hotmail.com>
2023-03-20 14:54:35 +0800

    [CHGLOG] 修改外头件

--- ui/simulator/inc/lv_main.h
+++ ui/simulator/inc/lv_main.h
@@ -1,6 +1,14 @@
 #ifndef _LV_MAIN_H_
 #define _LV_MAIN_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int lv_main(int argc, char *args[]);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif
--- ui/simulator/inc/lv_ui.h
+++ ui/simulator/inc/lv_ui.h
@@ -1,6 +1,14 @@
 #ifndef _LV_UI_H_
 #define _LV_UI_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void lv_ui(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif
